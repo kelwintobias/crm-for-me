77a68b123095f65054d4431651e442ab78f16bbd
kelwintobias <kelwintobiascomercial@gmail.com>
2026-01-25 09:13:14 -0300

    Fix duplicate import in dashboard-view.tsx

--- src/components/dashboard/dashboard-view.tsx
+++ src/components/dashboard/dashboard-view.tsx
@@ -21,7 +21,6 @@ import {
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-  DropdownMenuTrigger,
   DropdownMenuSeparator
 } from "@/components/ui/dropdown-menu";
 import { Debtor } from "../debtors/debtors-table";
